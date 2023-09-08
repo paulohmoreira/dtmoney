1c63f970208819cced70b33a7ae1a303c6c2269e
Paulo Moreira <phmoreira37@gmail.com>
2023-09-08 17:28:26 -0300

    feat: encapsulating Modal in a new component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 
 import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 
@@ -23,12 +24,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal 
+      <NewTransactionModal 
         isOpen={isNewTransactionModalOpen} 
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
 
       <GlobalStyle />
     </>

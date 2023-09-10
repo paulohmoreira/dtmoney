3d090a2ac5b7ae10a9c3dbfc52471dfa7c076db7
Paulo Moreira <phmoreira37@gmail.com>
2023-09-10 16:43:21 -0300

    feat: modal income and outcome structure

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg'
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg'
+import outComeImg from '../../assets/outcome.svg'
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
  isOpen: boolean;
@@ -26,6 +28,19 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <h2>Cadastrar transação</h2>
         <input type="text" placeholder='Título' />
         <input type="number" placeholder='Valor' />
+
+        <TransactionTypeContainer>
+          <button type='button'>
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type='button'>
+            <img src={outComeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input type="text" placeholder='Categoria' />
         <button type="submit">Cadastrar</button>
       </Container>

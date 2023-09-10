eef7e84fc6cede666af3500eddb419a0772b3624
Paulo Moreira <phmoreira37@gmail.com>
2023-09-10 18:28:39 -0300

    feat: getting and listing the table data from the api

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,28 @@ createServer({
   models: {
     transaction: Model,
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2023-07-16 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 1100,
+          createdAt: new Date('2023-07-21 19:00:00')
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
